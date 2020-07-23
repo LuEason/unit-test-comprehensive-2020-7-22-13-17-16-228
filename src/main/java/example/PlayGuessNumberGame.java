660c0a68154ec32ca7d47b85755e53f680b3b8b8
Eason <chenzscd@163.com>
2020-07-23 21:57:42 +0800

    [GuessNumberGame][Eason] feature: add PlayGuessNumberGame class and main method

---
+++ src/main/java/example/PlayGuessNumberGame.java
@@ -0,0 +1,33 @@
+package example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class PlayGuessNumberGame {
+
+    public static void main(String[] args) throws IOException {
+        Generator generator = new AnswerGenerator();
+        GuessNumberGame guessNumberGame = new GuessNumberGame(generator);
+        String result = "";
+        int time = 6;
+
+        while (time > 0 && !"4A0B".equals(result)) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("Please enter your answer:");
+            String guess = br.readLine();
+            if (guessNumberGame.validGuess(guess)) {
+                result = guessNumberGame.guess(guess);
+                System.out.println(result);
+            } else {
+                System.out.println("Wrong Input，Input again");
+            }
+            time --;
+        }
+        if ("4A0B".equals(result)) {
+            System.out.println("You win!");
+        } else {
+            System.out.println("You lose!");
+        }
+    }
+}
