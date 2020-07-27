6c7359cec0cfd69b61708791af12098b5f89fb84
Eason <chenzscd@163.com>
2020-07-27 23:11:22 +0800

    [GuessNumberGame][Eason] refactor: delete unused code

--- src/main/java/example/GuessNumberGame.java
+++ src/main/java/example/GuessNumberGame.java
@@ -14,25 +14,19 @@ public class GuessNumberGame {
     }
 
     public String guess(String guess) {
-        String result;
-        if (answer.equals(guess)) {
-            result = "4A0B";
-        } else {
-            int aNumber = 0;
-            int bNumber = 0;
-            for (int i = 0; i < guess.length(); i++) {
-                if (guess.charAt(i) == answer.charAt(i)) {
-                    aNumber ++;
-                } else {
-                    int indexInAnswerChars = answer.indexOf(guess.charAt(i));
-                    if (indexInAnswerChars != -1 && i != indexInAnswerChars) {
-                        bNumber ++;
-                    }
+        int aNumber = 0;
+        int bNumber = 0;
+        for (int i = 0; i < guess.length(); i++) {
+            if (guess.charAt(i) == answer.charAt(i)) {
+                aNumber ++;
+            } else {
+                int indexInAnswerChars = answer.indexOf(guess.charAt(i));
+                if (indexInAnswerChars != -1 && i != indexInAnswerChars) {
+                    bNumber ++;
                 }
             }
-            result = String.format("%sA%sB", aNumber, bNumber);
         }
-        return result;
+        return String.format("%sA%sB", aNumber, bNumber);
     }
 
     public void play() throws IOException {
