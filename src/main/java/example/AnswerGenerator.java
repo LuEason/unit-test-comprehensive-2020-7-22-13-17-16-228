9b274249a6f41ee06f801506e1b060d9819f253d
Eason <chenzscd@163.com>
2020-07-27 23:09:04 +0800

    [GuessNumberGame][Eason] refactor: add const ANSWER_LENGTH and MAXIMUM_ANSWER

--- src/main/java/example/AnswerGenerator.java
+++ src/main/java/example/AnswerGenerator.java
@@ -4,12 +4,16 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class AnswerGenerator implements Generator {
+
+    private static final int ANSWER_LENGTH = 4;
+    private static final int MAXIMUM_ANSWER = 10;
+
     @Override
     public String generate() {
         Set<String> answerCharSet = new HashSet<>();
         StringBuilder answer = new StringBuilder();
-        while(answerCharSet.size() < 4) {
-            answerCharSet.add(Integer.toString((int) Math.floor(Math.random() * 10)));
+        while(answerCharSet.size() < ANSWER_LENGTH) {
+            answerCharSet.add(Integer.toString((int) Math.floor(Math.random() * MAXIMUM_ANSWER)));
         }
         for (String string : answerCharSet) {
             answer.append(string);
