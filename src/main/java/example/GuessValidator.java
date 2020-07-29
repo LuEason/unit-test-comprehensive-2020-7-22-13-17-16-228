8be56736e8a2b236ffbf0fc57ecdbdb3e84fa5c3
Eason <chenzscd@163.com>
2020-07-29 10:14:41 +0800

    [GuessNumberGame][Eason] refactor: add const GUESS_NUMBER_LENGTH

--- src/main/java/example/GuessValidator.java
+++ src/main/java/example/GuessValidator.java
@@ -6,10 +6,11 @@ public class GuessValidator implements Validator {
 
     public static final String ONLY_NUMBER_REGEX = ".*[^0-9]+.*";
     public static final String NO_REPEAT_NUMBER_REGEX = ".*(.).*\\1.*";
+    public static final int GUESS_NUMBER_LENGTH = 4;
 
     @Override
     public boolean validate(String guess) {
-        if (guess.length() != 4) {
+        if (guess.length() != GUESS_NUMBER_LENGTH) {
             return false;
         }
         return !Pattern.matches(ONLY_NUMBER_REGEX, guess) && !Pattern.matches(NO_REPEAT_NUMBER_REGEX, guess);
